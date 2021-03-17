a3765e7a2f31a0f07c709ca645e279a37430f28f
pedro marval <marvalp.dev@gmail.com>
2021-03-17 17:02:44 -0400

    empezando con gulp

---
+++ Gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const rename = require('gulp-rename')
+
+gulp.task('style',()=>{
+    gulp
+        .src('index.scss')
+        .pipe(sass())
+        .pipe(rename('app.css'))
+        .pipe(gulp.dest('public'))
+})
+
+
+gulp.task('default' , ['styles'])
