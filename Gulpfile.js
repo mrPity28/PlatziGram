c581900718d104df05d11d6336ebb065a50b8254
pedro marval <marvalp.dev@gmail.com>
2021-03-29 16:43:16 -0400

    scripts gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const rename = require('gulp-rename')
+const babel = require('babelify')
+const broserify = require('browserify')
+const source = require('vinyl-source-stream')
+
 
 gulp.task('styles',()=>{
     return gulp
@@ -15,10 +19,19 @@ gulp.task('assets' , ()=>{
         .src('assets/*')
         .pipe(gulp.dest('public'))
 })
-
 // gulp.task('watch', () => {
 //     gulp.watch('app.scss', gulp.series('styles'));
 //     gulp.watch('asset/*', gulp.series('assets'));
 // });
 
-gulp.task('default', gulp.series('styles', 'assets'));
+gulp.task('scripts' , ()=>{
+    return broserify('./src/index.js')
+        .transform(babel)
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public'))
+})
+
+
+gulp.task('default', gulp.series('styles', 'assets' , 'scripts'));
